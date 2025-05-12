4c23c4bd396ed2302ae06a17ee4aa9e4969867c3
aodh66 <aodhan66@gmail.com>
2025-05-12 22:34:17 +0100

    set

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,68 @@ window.addEventListener("DOMContentLoaded", () => {
     greet();
   });
 });
+
+let params = {
+  // inputRequest: true,
+  inputFolder: "",
+  backupFolder: "",
+  snapshotFolder: "",
+  backupTime: 0,
+  backupNumber: 0,
+  
+}
+
+// TODO Could write smaller functions to handle sending the stuff for each button, 
+// TODO and this is just the aggregate handler that sends them to the backend
+function handleClick(event: Event) {
+  const target = event.target as HTMLElement
+  const id = target.id
+  console.log(target.id);
+  // TODO make this either into a clean looking one liner version or a switch statement like shown
+//   switch (id) {
+//     case "inputFolderBtn": processPending(); break;
+//     case "backupFolderBtn": processApproved(); break;
+//     default: handleError();
+// }
+    if (id == "inputFolderBtn") {
+      // request input folder
+        console.log("click");
+    } else
+    if (id == "backupFolderBtn") {
+      // request backup folder
+    } else
+    if (id == "snapshotFolderBtn") {
+      // request snapshot folder
+    } else
+    if (id == "backupBtn") {
+      // start backup
+    } else
+    if (id == "snapshotBtn") {
+      // take snapshot
+    } else
+    if (id == "snapshotHotkeyBtn") {
+      // register snapshot hotkey
+    } else
+    if (id == "newProfileBtn") {
+      // create new profile
+    } else
+    if (id == "saveProfileBtn") {
+      // save profile
+    } else
+    if (id == "loadProfileBtn") {
+      // load profile
+    }
+
+
+    
+}
+// const inputFolderBtn = document.querySelector("#inputFolderBtn")
+// inputFolderBtn?.addEventListener("click", handleClick("inputFolderBtn"))
+// document.querySelector("#inputFolderBtn")?.addEventListener("click", (event) => handleClick(
+//   // "inputFolderBtn",
+// event
+// ))
+document.querySelectorAll("button")?.forEach((button) => {
+  button.addEventListener("click", (event) => handleClick(event))
+})
+// TODO Could maybe add this event listener to all buttons in one go, pass in the event to handleClick
