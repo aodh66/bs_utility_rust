302898748674741248cb8a32a2f9e751010fbc99
aodh66 <aodhan66@gmail.com>
2025-05-15 18:42:02 +0100

    added os check when frontend loads

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,22 @@
 // Imports
-use tauri::ipc::Response;
 use rfd::FileDialog;
 use std::path::PathBuf;
+use tauri::ipc::Response;
+use tauri::{AppHandle, Emitter};
+
+// Check OS
+#[tauri::command]
+fn get_os() -> String {
+    let machine_kind = if cfg!(unix) {
+        "unix".to_string()
+    } else if cfg!(windows) {
+        "windows".to_string()
+    } else {
+        "unknown".to_string()
+    };
+    println!("OS type: {:?}", &machine_kind);
+    machine_kind
+}
 
 // Opens native os dialog for folder selection
 async fn folder_picker() -> Option<String> {
@@ -11,43 +26,14 @@ async fn folder_picker() -> Option<String> {
         .map(|path: PathBuf| path.to_string_lossy().to_string())
 }
 
-// #[tauri::command]
-// fn my_custom_command() {
-//     println!("I was invoked from JavaScript!");
-// }
-
-// Define a struct type that is just err: bool and message: String MAY OR MAY NOT BE USED
-// struct FolderResponse {
-//     err: bool,
-//     file_path: String,
-// }
-
-// Get a folder with native explorer
-// #[tauri::command]
-// fn get_folder(invoke_message: &str) -> Result<String, String> {
-//     println!(
-//         "Invoked from TypeScript, I am getting this folder: {}",
-//         invoke_message
-//     );
-//     // "Message from Rust".into();
-//     let test_bool = false;
-//     let path_fetch_err = "Test path fetch error".to_string();
-//     if !test_bool {
-//     //     // Ok(response.file_path);
-//     Ok("test/file/path".into())
-//     } else {
-//     //     // Err(response.message);
-//     Err(format!("Path fetch err: {}", path_fetch_err).into())
-//     }
-// }
-
 // An async function
 // Return a Result<String, ()> to bypass the borrowing issue
 #[tauri::command]
-async fn async_get_folder(invoke_message: &str) -> Result<String, String> {
-  // Call another async function and wait for it to finish
-  let folder = folder_picker().await;
-  // Note that the return value must be wrapped in `Ok()` now.
+// async fn async_get_folder(invoke_message: &str) -> Result<String, String> {
+async fn async_get_folder() -> Result<String, String> {
+    // Call another async function and wait for it to finish
+    let folder = folder_picker().await;
+    // Note that the return value must be wrapped in `Ok()` now.
     match folder {
         Some(path) => Ok(path.into()),
         // Some(path) => Err("Path fetch err".into()),
@@ -66,7 +52,11 @@ fn read_file() -> Response {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![async_get_folder, read_file])
+        .invoke_handler(tauri::generate_handler![
+            get_os,
+            async_get_folder,
+            read_file
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from '@tauri-apps/api/event';
 
 // console.log("hello");
+listen<string>('os-type', (event) => {
+    console.log(`OS is: ${event.payload}`);
+});
 
 window.addEventListener("DOMContentLoaded", () => {
 });
@@ -14,12 +18,27 @@ window.addEventListener("DOMContentLoaded", () => {
 // Write methods to update fields in this
 let params: { [key: string]: any } = {
     // inputRequest: true,
+    "os": "",
     "inputFolder": "",
     "backupFolder": "",
     "snapshotFolder": "",
     backupTime: 0,
     backupNumber: 0,
 }
+
+function getOS() {
+    // You can then call a method to update params where you are console.logging here
+    invoke('get_os', {})
+        .then((result: unknown) => {
+            const os = result as string | null; // Narrow the type to string | null
+            if (os != null) {
+                console.log(`OS is: ${os}`);
+                params.os = os; // Assign the value if it's not null
+            }
+        })
+        .catch((error) => console.error(error));
+}
+getOS();
 
 // TODO Could write smaller functions to handle sending the stuff for each button, 
 // TODO and this is just the aggregate handler that sends them to the backend
@@ -29,55 +48,59 @@ function handleClick(event: Event) {
     // console.log(target.id);
 
     // TODO make this either into a clean looking one liner version or a switch statement like shown
-    //   switch (id) {
-    //     case "inputFolderBtn": processPending(); break;
-    //     case "backupFolderBtn": processApproved(); break;
-    //     default: handleError();
-    // }
-    if (id == "inputFolderBtn") {
-        // request input folder
-        // You can then call a method to update params where you are console.logging here
-        asyncGetFolder("input");
-    } else
-        if (id == "backupFolderBtn") {
-            // request backup folder
-            asyncGetFolder("backup");
-        } else
-            if (id == "snapshotFolderBtn") {
-                // request snapshot folder
-                asyncGetFolder("snapshot");
-            } else
-                if (id == "backupBtn") {
-                    // start backup
-                } else
-                    if (id == "snapshotBtn") {
-                        // take snapshot
-                    } else
-                        if (id == "snapshotHotkeyBtn") {
-                            // register snapshot hotkey
-                        } else
-                            if (id == "newProfileBtn") {
-                                // create new profile
-                            } else
-                                if (id == "saveProfileBtn") {
-                                    // save profile
-                                } else
-                                    if (id == "loadProfileBtn") {
-                                        // load profile
-                                    }
-
-
-
+    switch (id) {
+        case "inputFolderBtn": asyncGetFolder("input"); break;
+        case "backupFolderBtn": asyncGetFolder("backup"); break;
+        case "snapshotFolderBtn": asyncGetFolder("snapshot"); break;
+        case "snapshotBtn": asyncSnapshot(); break;
+        default: handleError();
+    }
+    // if (id == "inputFolderBtn") {
+    //     // request input folder
+    //     // You can then call a method to update params where you are console.logging here
+    //     asyncGetFolder("input");
+    // } else
+    //     if (id == "backupFolderBtn") {
+    //         // request backup folder
+    //         asyncGetFolder("backup");
+    //     } else
+    //         if (id == "snapshotFolderBtn") {
+    //             // request snapshot folder
+    //             asyncGetFolder("snapshot");
+    //         } else
+    //             if (id == "backupBtn") {
+    //                 // start backup
+    //             } else
+    //                 if (id == "snapshotBtn") {
+    //                     // take snapshot
+    //                 } else
+    //                     if (id == "snapshotHotkeyBtn") {
+    //                         // register snapshot hotkey
+    //                     } else
+    //                         if (id == "newProfileBtn") {
+    //                             // create new profile
+    //                         } else
+    //                             if (id == "saveProfileBtn") {
+    //                                 // save profile
+    //                             } else
+    //                                 if (id == "loadProfileBtn") {
+    //                                     // load profile
+    //                                 }
 }
-// const inputFolderBtn = document.querySelector("#inputFolderBtn")
-// inputFolderBtn?.addEventListener("click", handleClick("inputFolderBtn"))
-// document.querySelector("#inputFolderBtn")?.addEventListener("click", (event) => handleClick(
-//   // "inputFolderBtn",
-// event
-// ))
+
+// Apply handleClick to all of the buttons
 document.querySelectorAll("button")?.forEach((button) => {
     button.addEventListener("click", (event) => handleClick(event))
 })
+
+// TODO error dropdown trigger fuction 
+function handleError() {
+    console.error("Button click handler error");
+    const backupMessageElement = document.querySelector(`#backupMessage`)
+    if (backupMessageElement) {
+        backupMessageElement.textContent = "Button click handler error"
+    }
+}
 
 // param should be 'input', 'backup', or 'snapshot'
 // function getFolder(invokeMessage: string) {
@@ -85,13 +108,13 @@ document.querySelectorAll("button")?.forEach((button) => {
 //   invoke('get_folder', { invokeMessage: invokeMessage })
 //       .then((result: unknown) => {
 //               const folder = result as string | null; // Narrow the type to string | null
-//               if (folder !== null) {
+//               if (folder != null) {
 //               console.log(folder);
 //               params[`${invokeMessage}Folder`]= folder; // Assign the value if it's not null
 //               const folderPathElement = document.querySelector(`#${invokeMessage}FolderPath`)
 //               if (folderPathElement) {
 //                   folderPathElement.textContent = folder ?? "No folder selected";
-//               }
+// }
 //           }
 //       })
 //       .catch((error) => console.error(error));
@@ -105,7 +128,7 @@ function asyncGetFolder(invokeMessage: string) {
     invoke('async_get_folder', { invokeMessage: invokeMessage })
         .then((result: unknown) => {
             const folder = result as string | null; // Narrow the type to string | null
-            if (folder !== null) {
+            if (folder != null) {
                 console.log(folder);
                 params[`${invokeMessage}Folder`] = folder; // Assign the value if it's not null
                 const folderPathElement = document.querySelector(`#${invokeMessage}FolderPath`)
@@ -115,6 +138,7 @@ function asyncGetFolder(invokeMessage: string) {
             }
         })
         .catch((error) => {
+            // TODO error dropdown trigger fuction 
             console.error(error)
             const backupMessageElement = document.querySelector(`#backupMessage`)
             if (backupMessageElement) {
@@ -123,4 +147,28 @@ function asyncGetFolder(invokeMessage: string) {
             }
 
         });
+}
+
+function asyncSnapshot() {
+    if (!params.inputFolder) {
+        // TODO error dropdown trigger fuction 
+        console.error("No input folder selected");
+        const backupMessageElement = document.querySelector(`#backupMessage`)
+        if (backupMessageElement) {
+            backupMessageElement.textContent = `No input folder selected`;
+        }
+    } else {
+        console.log(params.inputFolder.lastIndexOf("/"));
+
+        let finalFolder = params.inputFolder.slice(params.inputFolder.lastIndexOf("/"));
+        console.warn("DEBUGPRINT[11]: main.ts:142: finalFolder=", finalFolder)
+
+    }
+    // console.log(params.inputFolder);
+
+
+    // let snapshotName = `${} snapshot`;
+    // if (snapshotName) {
+    //
+    // }
 }
